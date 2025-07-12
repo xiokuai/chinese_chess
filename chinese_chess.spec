52f5418a637737eb428c0a59b9b97a5ee73058ca
aba2222 <ABCSSS2021@outlook.com>
2025-07-12 16:43:39 +0800

    更改安装包设置

--- chinese_chess.spec
+++ chinese_chess.spec
@@ -35,5 +35,6 @@ exe = EXE(
     target_arch=None,
     codesign_identity=None,
     entitlements_file=None,
+    uac_admin=True,
     icon=['chinese_chess\\logo.ico'],
 )
--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @echo off
-pyinstaller -F chinese_chess\main.py -n chinese_chess --icon=chinese_chess\logo.ico
+pyinstaller -F chinese_chess\main.py -n chinese_chess --icon=chinese_chess\logo.ico --uac-admin
 xcopy chinese_chess\audio dist\audio /E /I /H /Y
 xcopy chinese_chess\audio dist\data /E /I /H /Y
 copy x64\Release\alpha_beta_search.dll dist
