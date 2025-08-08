433a646bc31fc9db78410d41eea59fe8257b28ce
aba2222 <ABCSSS2021@outlook.com>
2025-08-08 21:12:25 +0800

    发生错误时打印traceback

--- chinese_chess/main.py
+++ chinese_chess/main.py
@@ -1,26 +1,3 @@
-"""
-* main.py:            程序入口
-
-### 源代码文件
-
-1. AI.py:              电脑算法
-2. configure.py:       配置设定
-3. constants.py:       所有常量
-4. GUI.py:             图形界面
-5. LAN.py:             局域网功能
-6. rule.py:            游戏规则控制
-7. tkintertools.py:    图形界面辅助模块
-8. tools.py:           工具函数
-
-### 资源文件
-
-1. chnese_chess_config.json:    配置信息
-2. chenese_chess_statistic.json: 统计数据
-3. help.md:        帮助文本
-4. audio/*.wav:    音效文件
-5. data            棋局库
-"""
-
 # 版本号
 __version__ = '1.1.0'
 # 作者
@@ -32,8 +9,9 @@ import tkinter as tk
 from tkinter import messagebox
 import sys
 
-def show_error_popup(exc_type, exc_value, _exc_traceback):
+def show_error_popup(exc_type, exc_value, exc_traceback):
     # TODO: Logging
+    print(exc_traceback)
     root = tk.Tk()
     root.withdraw()
     short_msg = f"程序发生错误：{exc_type.__name__}: {exc_value}\n位于{exc_value.__traceback__.tb_frame.f_code.co_filename}的第{exc_value.__traceback__.tb_lineno}行"
